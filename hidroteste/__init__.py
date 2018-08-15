68e041dfb105799027f63c44e3aaa3e1b86519a3
Marcos <marcos_lrodrigues@yahoo.com.br>
2018-08-15 16:50:46 -0300

    Adição de testes

--- hidroteste/__init__.py
+++ hidroteste/__init__.py
@@ -3,3 +3,6 @@
 __version__ = '0.0.1'
 __author__ = 'Marcos Rodrigues <marcos_lrodrigues@yahoo.com.br>'
 __all__ = []
+
+import logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -2,3 +2,9 @@
 
 def test_pass():
     assert True, "dummy sample test"
+
+from hidroteste.core import MyClass
+
+def test_get_5():
+    myclass = MyClass()
+    assert myclass.get_5() == 5, "MyClass get_5 returns 5"
